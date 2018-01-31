3e8dc9c3f496eed6df9a723c21ecfe7e7fb14a44
pzinsta <voodooshake@gmail.com>
2018-01-31 21:10:36 +0200

    Add setter injection with autowiring

--- src/main/java/com/pzinsta/springdemo/TennisCoach.java
+++ src/main/java/com/pzinsta/springdemo/TennisCoach.java
@@ -8,10 +8,19 @@ public class TennisCoach implements Coach {
 
     private FortuneService fortuneService;
 
+    public TennisCoach() {
+        System.out.println("Default constructor");
+    }
+    
     @Autowired
-    public TennisCoach(FortuneService fortuneService) {
+    public void setFortuneService(FortuneService fortuneService) {
+        System.out.println("Setter");
         this.fortuneService = fortuneService;
     }
+//    @Autowired
+//    public TennisCoach(FortuneService fortuneService) {
+//        this.fortuneService = fortuneService;
+//    }
     
     @Override
     public String getDailyWorkout() {
