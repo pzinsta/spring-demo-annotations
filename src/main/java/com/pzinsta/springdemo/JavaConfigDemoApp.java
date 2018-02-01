b70acf8fd0d6eb834ec8e965a015e07c1fdf72e9
pzinsta <voodooshake@gmail.com>
2018-02-01 17:22:22 +0200

    Add annotation config demo

---
+++ src/main/java/com/pzinsta/springdemo/JavaConfigDemoApp.java
@@ -0,0 +1,21 @@
+package com.pzinsta.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class JavaConfigDemoApp {
+
+    public static void main(String[] args) {
+        // read spring config file
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+        
+        // get the bean from spring container
+        Coach coach = context.getBean("tennisCoach", Coach.class);
+        
+        // call a method on the bean
+        System.out.println(coach.getDailyWorkout());
+        System.out.println(coach.getDailyFortune());
+        
+        // close the context
+        context.close();
+    }
+}
---
+++ src/main/java/com/pzinsta/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.pzinsta.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.pzinsta.springdemo")
+public class SportConfig {
+
+}
